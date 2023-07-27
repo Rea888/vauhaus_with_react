130ccbfde790d9f2148458b1ee146e67c17abfb6
rea <m.rakhely.viktoria@gmail.com>
2023-07-27 13:14:50 +0200

    topbanner

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-
-<link rel="icon" href="%PUBLIC_URL%/logovauhouse-removebg-preview.png" />
+import TopBanner from "../../component/TopBanner";
 
 
 const App = () => {
     return (
         <div>
-            <h1>Larvael with vite and React</h1>
+            <TopBanner /> 
         </div>
     )
 }
