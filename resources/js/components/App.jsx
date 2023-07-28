657f0ce78e29f6d5aeeb0c151db6d4fde7427fee
rea <m.rakhely.viktoria@gmail.com>
2023-07-28 14:00:26 +0200

    Created navbar

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import TopBanner from "../../component/TopBanner";
 import Service from "../../component/Service";
+import TopNavigation from "../../component/TopNavigation";
 
 
 const App = () => {
     return (
         <div>
+            <TopNavigation />
             <TopBanner />
-            <Service /> 
+            <Service />
+             
         </div>
     )
 }
